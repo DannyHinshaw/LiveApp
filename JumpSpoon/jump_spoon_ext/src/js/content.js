f97a9d94a96294cca895edc81692cc38229c4d13
Danny <danny@nulleffort.com>
2017-07-29 16:52:51 -0400

    loading all iframes

--- JumpSpoon/jump_spoon_ext/src/js/content.js
+++ JumpSpoon/jump_spoon_ext/src/js/content.js
@@ -1,7 +1,7 @@
 /*
-import handlers from './modules/handlers';
-import msg from './modules/msg';
-*/
+ import handlers from './modules/handlers';
+ import msg from './modules/msg';
+ */
 // here we use SHARED message handlers, so all the contexts support the same
 // commands. but this is NOT typical messaging system usage, since you usually
 // want each context to handle different commands. for this you don't need
@@ -21,110 +21,111 @@ console.log('CONTENT SCRIPT WORKS!'); // eslint-disable-line no-console
  console.log('jQuery version:', $().jquery); // eslint-disable-line no-console
  */
 
+window.addEventListener('load', (event) => {
+  const form = {
 
-const form = {
-
-  // Custom CSS Styles
-  templates: {
-    iframeContainer: `<div id='iframeContainer'></div>`,
-    instructions   : `<div class='toggle-instructions'>` + // eslint-disable-line key-spacing
-                        `<span class='toggle-icon'> + </span> Click to toggle instructions
+    // Custom CSS Styles
+    templates: {
+      iframeContainer: `<div id='iframeContainer'></div>`,
+      instructions   : `<div class='toggle-instructions'>` + // eslint-disable-line key-spacing
+      `<span class='toggle-icon'> + </span> Click to toggle instructions
                       </div>
                       <br>`,
-    venueIdClipped : `<span style='color: #61ca61; display: block;'>` +  // eslint-disable-line key-spacing
-                       `<br>Copied to clipboard!
+      venueIdClipped : `<span style='color: #61ca61; display: block;'>` +  // eslint-disable-line key-spacing
+      `<br>Copied to clipboard!
                       </span>`
-  },
+    },
 
-  /*
-   addCSS(styleSheet) {
-   const style = document.createElement('style');
-   style.type = 'text/css';
-   style.appendChild(document.createTextNode(styleSheet));
-   document.head.appendChild(style);
-   },
-   */
+    clearInstructions() {
+      const formName = document.querySelector('.formName');
+      formName.insertAdjacentHTML('afterend', this.templates.instructions);
 
-  clearInstructions() {
-    const formName = document.querySelector('.formName');
-    formName.insertAdjacentHTML('afterend', this.templates.instructions);
+      // Remove instruction block and assign toggle function
+      document.querySelector('.formDescription').style.display = 'none';
+      document.querySelector('.toggle-instructions').onclick = this.toggleInstructions;
+    },
 
-    // Remove instruction block and assign toggle function
-    document.querySelector('.formDescription').style.display = 'none';
-    document.querySelector('.toggle-instructions').onclick = this.toggleInstructions;
-  },
+    toggleInstructions() {
+      const header = document.querySelector('.formHeader');
+      const instructions = document.querySelector('.formDescription');
+      const toggler = document.querySelector('.toggle-instructions');
+      const toggleIcon = toggler.querySelector('.toggle-icon');
+      toggler.classList.toggle('open');
 
-  toggleInstructions() {
-    const header = document.querySelector('.formHeader');
-    const instructions = document.querySelector('.formDescription');
-    const toggler = document.querySelector('.toggle-instructions');
-    const toggleIcon = toggler.querySelector('.toggle-icon');
-    toggler.classList.toggle('open');
+      if (toggler.classList.contains('open')) {
+        header.style.height = '20rem';
+        instructions.style.display = 'block';
+        toggler.style.backgroundColor = '#97ceea';
+        toggleIcon.innerHTML = ' - ';
+      } else {
+        header.style.height = 'initial';
+        instructions.style.display = 'none';
+        toggler.style.backgroundColor = '#97eaa2';
+        toggleIcon.innerHTML = ' + ';
+      }
+    },
 
-    if (toggler.classList.contains('open')) {
-      header.style.height = '20rem';
-      instructions.style.display = 'block';
-      toggler.style.backgroundColor = '#97ceea';
-      toggleIcon.innerHTML = ' - ';
-    } else {
-      header.style.height = 'initial';
-      instructions.style.display = 'none';
-      toggler.style.backgroundColor = '#97eaa2';
-      toggleIcon.innerHTML = ' + ';
+    descriptionLimit() {
+      /* eslint no-param-reassign: ["error", { "props": false }] */
+      // Fixes formatting where descriptions are too long and screw up grid
+      return [...document.querySelectorAll('.description')].forEach((d) => {
+        d.textContent.length > 52 // eslint-disable-line no-unused-expressions
+          ? d.parentElement.style.display = 'block'
+          : null;
+      });
+    },
+
+    removeUnwantedElements() {
+      [document.querySelector('.formCoverImageContainer'),
+        ...document.querySelectorAll('.createYourOwnFormWithAirtable')]
+        .forEach(el => el.remove());
+    },
+
+    createIframe(type, link) {
+      const iframe = document.createElement('iframe');
+      iframe.className = `iframe-stacked ${type}`;
+      iframe.src = link.startsWith('http') ? link : `http://${link}`;
+      return iframe;
+    },
+
+    generateIframes(container) {
+      const _getType = n => n.closest('.sharedFormField').firstElementChild.textContent.split('Venue ')[1].trim();
+
+      [...document.querySelectorAll('.detailViewTextWithLinks a')].forEach((link, i) => {
+        if (i === 3) {
+          // May change in the future. For now this is the recurring event form link
+          return Object.defineProperty(form, 'recurEventLink', { value: link.href });
+        }
+        return link.href ? container.prepend(this.createIframe(_getType(link), link.href)) : null;
+      });
+    },
+
+    setIframes() {
+      const formContainer = document.querySelector('.formFieldAndSubmitContainer');
+      const formHeader = document.querySelector('.formHeader');
+
+      formContainer.prepend(formHeader);
+      formContainer.insertAdjacentHTML('afterend', this.templates.iframeContainer);
+
+      // Methods for dynamically generating links
+      return this.generateIframes(document.getElementById('iframeContainer'));
+    },
+
+    init() {
+      // DOM JS Methods need to wait shortly after window for load
+      setTimeout(() => {
+        this.clearInstructions();
+        this.removeUnwantedElements();
+        this.descriptionLimit();
+        this.setIframes();
+      }, 200);
     }
-  },
+  };
+  console.log(event); // eslint-disable-line no-console
 
-  descriptionLimit() {
-    /* eslint no-param-reassign: ["error", { "props": false }] */
-    // Fixes formatting where descriptions are too long and screw up grid
-    return [...document.querySelectorAll('.description')].forEach((d) => {
-      d.textContent.length > 52 // eslint-disable-line no-unused-expressions
-        ? d.parentElement.style.display = 'block'
-        : null;
-    });
-  },
-
-  removeUnwantedElements() {
-    [document.querySelector('.formCoverImageContainer'),
-      ...document.querySelectorAll('.createYourOwnFormWithAirtable')]
-      .forEach(el => el.remove());
-  },
-
-  setIframe() {
-    const formContainer = document.querySelector('.formFieldAndSubmitContainer');
-    const formHeader = document.querySelector('.formHeader');
-
-    // this.wrapAll(formEls, wrapperDiv);
-    // this.addCSS(this.css.splitView);
-    formContainer.prepend(formHeader);
-    formContainer.insertAdjacentHTML('afterend', this.templates.iframeContainer);
-
-    // const test = this.ajax('https://jsfiddle.net/');
-    // console.log(test);
-    // TODO: This is just a temp placeholder
-
-    const iframe = document.createElement('iframe');
-    iframe.id = 'testIframe';
-    iframe.src = document.querySelectorAll('.detailViewTextWithLinks a')[0].href;// testthis.ajax('http://test.com');// 'https://jsbin.com';
-    document.getElementById('iframeContainer').prepend(iframe);
-  },
-
-  init() {
-    // this.addCSS(this.css.main);
-    // DOM JS Methods need to wait shortly for load
-    setTimeout(() => {
-      this.clearInstructions();
-      this.removeUnwantedElements();
-      this.descriptionLimit();
-      // this.getVenueID();
-      this.setIframe();
-    }, 250);
-  }
-
-};
-
-// Initialize settings
-console.log('running'); // eslint-disable-line no-console
-const formFactoryFn = () => Object.create(form);
-const formify = formFactoryFn();
-formify.init();
+  // Initialize settings
+  // Prototypal object creation with object factory function
+  const formFactoryFn = () => Object.create(form);
+  const formify = formFactoryFn();
+  return formify.init();
+});
